9abb5019e1638b393e390b12625f66f1e637d6bd
Damian Kilian <kilianpoczta@gmail.com>
2024-08-21 12:59:48 +0200

    FilterFactory

---
+++ database/factories/FilterOptionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FilterOption>
+ */
+class FilterOptionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = ucfirst(fake()->words(2, true));
+        $datetime = fake()->dateTimeBetween('-1 month', 'now');
+        return [
+            'name' => $name,
+            'order_priority' => fake()->numberBetween(0, 100000),
+            'created_at' => $datetime,
+            'updated_at' => $datetime,
+        ];
+    }
+}
