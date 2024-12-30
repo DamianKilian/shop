7563225429dddde99208d522ef749b2cb617e0fe
Damian Kilian <kilianpoczta@gmail.com>
2024-12-30 12:40:51 +0100

    simple tests

--- tests/Feature/MainTest.php
+++ tests/Feature/MainTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -9,9 +11,57 @@ class MainTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_category(): void
+    {
+        $slug = 'slug';
+        Category::factory()->create([
+            'slug' => $slug,
+        ]);
+
+        $response = $this->get("/category/$slug");
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_getProductsView(): void
+    {
+        $response = $this->post('/api/get-products-view');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_getProductsViewAllCategories(): void
+    {
+        $response = $this->post('/api/get-products-view-all-categories');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_getProductNums(): void
+    {
+        $response = $this->post('/api/get-product-nums');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_index(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_product(): void
+    {
+        $slug = 'slug';
+        $category = Category::factory()->create();
+        Product::factory()->create([
+            'slug' => $slug,
+            'category_id' => $category->id,
+        ]);
+
+        $response = $this->get("/product/$slug");
 
         $response->assertStatus(200);
     }
