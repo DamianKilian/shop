2ee3236b44e66bb8fb6c8b883a51352820116ecf
Damian Kilian <kilianpoczta@gmail.com>
2025-01-18 18:00:37 +0100

    admin permissions test

--- tests/Feature/Authz/PermissionsTest.php
+++ tests/Feature/Authz/PermissionsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -9,8 +10,18 @@ class PermissionsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_category()
+    public function test_admin_can_access_admin_panel()
     {
-        
+        $response = $this->actingAs(parent::getAdmin())->get('/admin-panel/products');
+
+        $response->assertSuccessful();
+    }
+
+    public function test_not_admin_can_not_access_admin_panel()
+    {
+        $user = User::factory()->make();
+        $response = $this->actingAs($user)->get('/admin-panel/products');
+
+        $response->assertStatus(403);
     }
 }
