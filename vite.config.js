6e693b1c91e5ead617a166c6c29516b6df33b87a
Damian Kilian <kilianpoczta@gmail.com>
2025-02-06 10:42:57 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 import { glob } from 'glob';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+    },
     plugins: [
         laravel({
             input: [
